aee9ced4fb97432122c98f08001a256a26fc9375
shingoed <nakajima.shingo@gmail.com>
2020-01-21 12:16:11 -0800

    finished except for tests

---
+++ src/test/java/com/shingo/songr/AlbumTest.java
@@ -0,0 +1,31 @@
+package com.shingo.songr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumTest {
+
+    @Test
+    void getTitle() {
+
+
+
+    }
+
+    @Test
+    void getArtist() {
+    }
+
+    @Test
+    void getSongCount() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void getImageUrl() {
+    }
+}
